f1a56ee8107bd45704f28f2aaf9d377184be0736
da-the-dev <leshiktk@me.com>
2020-09-17 23:02:05 +0300

    A few additions to commands and tournament class

--- index.js
+++ index.js
@@ -11,17 +11,44 @@ client.once('ready', () => {
 class Tournament {
     /**@param {string} name Tournament's name */
     constructor(name) {
-        /**@type Name of the tournament */
+        /**@type {string} Name of the tournament */
         this.name = name
+        /**@type {Array<Discord.GuildMember>} Array of tournament members */
+        this.members = new Array
+        /**@type {Array<Array<Discord.GuildMember>>} Tournament members in order */
+        this.tournament = new Array
+    }
+    /**@param {Discord.GuildMember} member Add a member to paticipants list */
+    addMember(member) {
+        this.members.push(member)
     }
 }
+/**@type {Tournament} */
 var tournament
 client.on("message", (message) => {
     var msg = new pm(message)
     if(msg.prefix == '!' && !msg.msg.author.bot)
         switch(msg.command) {
             case "start":
-                var tournament = new Tournament
+                if(msg.args[0])
+                    tournament = new Tournament(msg.args[0])
+                else
+                    msg.msg.reply('no tournament name specified'); return
+                break
+            case "addMember":
+                var memberMentioned = msg.msg.mentions.members.first()
+                if(memberMentioned)
+                    tournament.addMember(msg.msg.mentions.members.first())
+                else
+                    msg.msg.reply('invalid member'); return
+                break
+            case "ping":
+                msg.msg.reply(client.ws.ping)
+                break
+            case "start":
+                break
+            case "postMembers":
+                console.log(tournament.members)
                 break
             default:
                 msg.msg.reply('unknown command')
